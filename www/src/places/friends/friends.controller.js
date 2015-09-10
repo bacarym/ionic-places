c75fba52b9e1b7c593669e5ad47e913a83c889e5
bacary mbaye <bacarymbaye@MacBook-Pro-de-bacary.local>
2015-09-10 11:35:46 +0200

    Friends

--- www/src/places/friends/friends.controller.js
+++ www/src/places/friends/friends.controller.js
@@ -1,6 +1,17 @@
 angular.module('places')
-	.controller('FriendsCtrl', function($scope, $ionicLoading,FriendsService) {
+	.controller('FriendsCtrl', function($scope, $ionicLoading,FriendsService,$http){
 
+	$http.get("/api/friends.json").
+		success(function(data, status, header, config){
+			console.log(data)
+
+			
+			$scope.list = data;
+		}).
+		error(function(data, status, header, config){
+			console.log("error")
+
+		});
 
 
 	});
